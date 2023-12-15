0a8c62307a7a15113da19513d3a8a3616f9c0b37
Piotr Wojenski <pwojenskip@gmail.com>
2023-12-15 17:56:08 +0100

    Add navigation links to Navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
 import Home from './pages/Home/Home'
+import Products from './pages/Products'
+import Cart from './pages/Cart'
 
 function App() {
 	return (
