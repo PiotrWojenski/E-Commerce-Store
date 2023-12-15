f1c56151c5392a1c0e45170ff564d4b18cf13bcb
Piotr Wojenski <pwojenskip@gmail.com>
2023-12-15 21:18:52 +0100

    Update fonts and logo in HTML and CSS files

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
+import logo from '../../assets/SHOPski.svg'
 
 const Navbar = () => {
 	return (
 		<nav className="navbar">
+			<div className="logo-container">
+				<img src={logo} alt="Logo" className="logo" />
+			</div>
 			<ul className="nav-list">
 				<li>
 					<Link to="/">Home</Link>
