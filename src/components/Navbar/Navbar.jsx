d1227218d862c51742ba7f4d0108c7bfece162fa
Piotr Wojenski <pwojenskip@gmail.com>
2023-12-16 17:01:03 +0100

    Refactor Navbar component with styled-components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,63 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './Navbar.css'
+import styled from 'styled-components'
 import logo from '../../assets/SHOPski.svg'
 
-const Navbar = () => {
+const NavbarContainer = styled.nav`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 15px;
+	background-color: #333;
+`
+
+const LogoContainer = styled.div`
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+`
+
+const LogoImage = styled.img`
+	width: 50px;
+	height: auto;
+`
+
+const NavList = styled.ul`
+	list-style: none;
+	margin: 0;
+	padding: 0;
+	display: flex;
+	justify-content: flex-end;
+`
+
+const NavListItem = styled.li`
+	margin-right: 20px;
+`
+
+const NavLink = styled(Link)`
+	text-decoration: none;
+	color: white;
+`
+
+function MyNavbar() {
 	return (
-		<nav className="navbar">
-			<div className="logo-container">
-				<img src={logo} alt="Logo" className="logo" />
-			</div>
-			<ul className="nav-list">
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/products">Products</Link>
-				</li>
-				<li>
-					<Link to="/cart">Cart</Link>
-				</li>
-			</ul>
-		</nav>
+		<NavbarContainer>
+			<LogoContainer>
+				<LogoImage src={logo} alt="Logo" />
+			</LogoContainer>
+			<NavList>
+				<NavListItem>
+					<NavLink to="/">Home</NavLink>
+				</NavListItem>
+				<NavListItem>
+					<NavLink to="/products">Products</NavLink>
+				</NavListItem>
+				<NavListItem>
+					<NavLink to="/cart">Cart</NavLink>
+				</NavListItem>
+			</NavList>
+		</NavbarContainer>
 	)
 }
 
-export default Navbar
+export default MyNavbar
