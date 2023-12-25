b33e43d16e767858d2a616d69804bd48c728617b
Piotr Wojenski <pwojenskip@gmail.com>
2023-12-25 22:37:52 +0100

    Add paragraph to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,11 @@ const Home = () => {
 	return (
 		<div>
 			<h1>Welcome to the Home Page</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium odio molestias sint expedita sapiente
+				tempora, libero exercitationem provident beatae reiciendis explicabo qui, optio eaque nostrum est maiores rerum
+				impedit saepe.
+			</p>
 			<p>This is the content of the home page.</p>
 		</div>
 	)
