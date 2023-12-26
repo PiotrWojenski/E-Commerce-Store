e7c646798a7438840f2c70f7669664d811292fcb
Piotr Wojenski <pwojenskip@gmail.com>
2023-12-26 20:42:23 +0100

    Refactor Slider component and add Slider to Home page

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Slider = () => {
-	return <div>Slider</div>
+	return (
+		<div>
+			<div></div>
+		</div>
+	)
 }
 
 export default Slider
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
+import Slider from '../../components/Slider'
 
 const Home = () => {
 	return (
 		<div>
 			<h1>Welcome to the Home Page</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium odio molestias sint expedita sapiente
-				tempora, libero exercitationem provident beatae reiciendis explicabo qui, optio eaque nostrum est maiores rerum
-				impedit saepe.
-			</p>
-			<p>This is the content of the home page.</p>
+			<Slider />
 		</div>
 	)
 }
